735e6f1d198d8d17f220dd38ba90c4ac10c7c414
pingu111 <theo.carron@telecom-sudparis.eu>
2017-08-11 23:36:12 +0200

    ADDED : generation of bricks

---
+++ BrickBreaker/Assets/GameCreator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameCreator : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_PrefabBrick = null;
+
+    [SerializeField]
+    private GameObject m_BrickContainer = null;
+
+    public void CreateGame()
+    {
+        if (m_PrefabBrick == null)
+        {
+            Debug.LogError("No brick prefab atttached");
+            return;
+        }
+
+        if (m_BrickContainer == null)
+        {
+            Debug.LogError("No brick container atttached");
+            return;
+        }
+
+        // first, set the size of the container to fit the screen as wanted
+        float wantedWidth = 1.0f * Camera.main.orthographicSize * 2.0f * Screen.width / Screen.height;
+        float wantedHeight = 0.5f * Camera.main.orthographicSize * 2.0f * Screen.height / Screen.width;
+        m_BrickContainer.transform.localScale = new Vector3(wantedWidth, wantedHeight, 1);
+
+        // then, place it on the screen
+        m_BrickContainer.transform.localPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width * 0.5f, Screen.height * 0.6f, 0)); ;
+
+        CreateBricks();
+    }
+
+    private void CreateBricks()
+    {
+        //TODO : add in a file
+        int nbBricksX = 10;
+        int nbBricksY = 6;
+
+        if (nbBricksX == 0 || nbBricksY == 0)
+            return;
+
+        Vector3 scaleOneBrick = new Vector3(m_BrickContainer.transform.localScale.x / (float)nbBricksX, m_BrickContainer.transform.localScale.y / (float)nbBricksY, 1);
+        float offsetX = m_PrefabBrick.GetComponent<MeshRenderer>().bounds.size.x * scaleOneBrick.x / m_PrefabBrick.transform.localScale.x;
+        float offsetY = m_PrefabBrick.GetComponent<MeshRenderer>().bounds.size.y * scaleOneBrick.y / m_PrefabBrick.transform.localScale.y;
+        for (int i = 0; i < nbBricksX; i ++)
+        {
+            for (int j = 0; j < nbBricksY; j++)
+            {
+                GameObject newBrick = Instantiate(m_PrefabBrick);
+                newBrick.transform.localScale = scaleOneBrick;
+
+                newBrick.transform.localPosition = new Vector3(
+                    m_BrickContainer.GetComponent<BoxCollider>().bounds.size.x * 0.5f - (i +0.5f) * offsetX,
+                    m_BrickContainer.GetComponent<BoxCollider>().bounds.size.y *0.5f - (j - 0.5f) * offsetY,
+                    m_BrickContainer.transform.position.z);
+                newBrick.transform.SetParent(m_BrickContainer.transform);
+
+                newBrick.GetComponent<MeshRenderer>().material.color = new Color(UnityEngine.Random.Range(0, 100)/100.0f, UnityEngine.Random.Range(0, 100)/100.0f, UnityEngine.Random.Range(0, 100)/100.0f);
+
+            }
+        }
+
+    }
+
+}
---
+++ BrickBreaker/Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameCreator m_GameCreator = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    if(m_GameCreator == null)
+        {
+            Debug.LogError("No GameCreator attached");
+            return;
+        }
+
+        m_GameCreator.CreateGame();
+    }
+}
