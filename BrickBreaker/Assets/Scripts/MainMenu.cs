3ccb1f50457f843f2e22a36d505480412933b403
pingu111 <theo.carron@telecom-sudparis.eu>
2017-08-11 22:19:42 +0200

    ADDED : beginning project, switch scenes working

---
+++ BrickBreaker/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Manager of the main menu, and its inputs
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// To call when the player click on Play button
+    /// </summary>
+    public void OnClickOnLaunchGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+    }
+
+    /// <summary>
+    /// To call when the player changes its settings about
+    /// the use of gyro
+    /// </summary>
+    /// <param name="newValue"></param>
+    public void OnValueGyroscopeChanged(bool newValue)
+    {
+        PlayerPreferences.m_PlayerWantsGyroscope = newValue;
+    }
+
+    /// <summary>
+    /// To call when the player wants to quit the applciation
+    /// </summary>
+    public void OnClickQuitApplication()
+    {
+        Application.Quit();
+    }
+}
---
+++ BrickBreaker/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Preferences of the player, its settings
+/// </summary>
+public static class PlayerPreferences
+{
+    public static bool m_PlayerWantsGyroscope = false;
+}
+
+/// <summary>
+/// Statistics of the player : number of points, ...
+/// </summary>
+public static class PlayerStatistics
+{
+    public static int PlayerNbPoints = 0;
+}
+
+public class PlayerManager : MonoBehaviour
+{
+
+}
